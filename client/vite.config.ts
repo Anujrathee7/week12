ba49708c0fb43db89bf86718ebbc6a3d6dd71a14
Anujrathee7 <anujrathee48@gmail.com>
2025-08-03 17:15:51 +0300

    debug

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1234',
+        changeOrigin: true,
+      }
+    }
+  }
 })
